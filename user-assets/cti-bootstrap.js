b9c21c608746f12e1ad0a5f72c793e44229dc3e9
paulsmith47 <paul.smith@leics-his.nhs.uk>
2019-01-16 14:40:56 +0000

    File added from CommonTime Infinity

---
+++ user-assets/cti-bootstrap.js
@@ -0,0 +1,33 @@
+window.goBack = function() {
+
+    window.location.href = "index.html#/app/ChatHealthIM/config";
+}
+
+setTimeout(function () {
+
+    var nodes = document.body.childNodes;
+
+    for(var i = 0; i < nodes.length; i++) {
+
+        if (nodes[i].nodeName === "DIV") {
+
+            nodes[i].parentElement.removeChild(nodes[i]);
+        }
+    }
+
+    var container = document.createElement("div");
+    container.id = "body";
+
+    document.body.appendChild(container);
+
+    const trigger = function() {
+        if (typeof wc === "undefined") {
+            setTimeout(trigger, 200);
+            return;
+        }
+
+        wc.triggerBootstrap();
+    };
+
+    trigger();
+}, 500);
